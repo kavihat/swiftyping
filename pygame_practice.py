6065d2a49638a1af3a5574656b6496290087388b
Kavitha <kavita.kannanunny@gmail.com>
2020-06-28 19:55:05 -0400

    Added functions and background image to the window

--- pygame_practice.py
+++ pygame_practice.py
@@ -1,4 +1,5 @@
 import pygame, random
+from pygame.locals import *
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 850
@@ -7,6 +8,7 @@ blue = (30, 30, 255)
 green = (30, 150, 30)
 
 pygame.init()
+font = pygame.font.SysFont('Arial', 70, bold=True)
 
 
 # floating words
@@ -18,42 +20,59 @@ def floating_words():
     lines = open('wordlist.txt').read().splitlines()
     chosen_word = random.choice(lines)
     txt = font.render(chosen_word, True, blue)
-    return txt, w_x, w_y,speed
+    return txt, w_x, w_y, speed
 
 
-# Window creation
-window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-window.fill(bcg)
-pygame.display.set_caption('SWIFTyping')
-a = pygame.image.load('icon.png')
-pygame.display.set_icon(a)
+# Main Window creation with background image
+def window_creation():
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    window.fill(bcg)
+    pygame.display.set_caption('SWIFTyping')
+    a = pygame.image.load('icon.png')
+    pygame.display.set_icon(a)
+    # background and text creation
+    background = pygame.image.load('lightkey.jpg').convert()
+    window.blit(background, (0, 0))
+    text = font.render('SWIFTyping Test', True, (255, 0, 0))
+    sprite = pygame.sprite.Sprite()
+    sprite.image = pygame.Surface([700, 100]).convert()
+    sprite.rect = (SCREEN_WIDTH / 2 - 250, 0)
+    sprite.image.set_colorkey((0, 0, 0))
+    sprite.image.blit(text, (0, 0))
+    window.blit(sprite.image, sprite.rect)
+    pygame.display.flip()
+    return window
 
-# background and text creation
-# background = pygame.image.load('lightkey.jpg').convert()
-# window.blit(background, (0, 0))
-font = pygame.font.SysFont('Arial', 70, bold=True)
-# text = font.render('SWIFTyping Test', True, (255, 0, 0))
-# sprite = pygame.sprite.Sprite()
-# sprite.image = pygame.Surface([700, 100]).convert()
-# sprite.rect = (SCREEN_WIDTH / 2 - 250, 0)
-# sprite.image.set_colorkey((0, 0, 0))
-# sprite.image.blit(text, (0, 0))
-# window.blit(sprite.image, sprite.rect)
-pygame.display.flip()
 
-text, word_x,word_y, speed = floating_words()
+# Game window creation
+text, word_x, word_y, speed = floating_words()
+
+
+def game_window():
+    run = True
+    word_y = 0
+    while run:
+
+        window.fill(bcg)
+        word_y += speed
+        window.blit(text, (word_x, word_y))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:   # press escape key to come back to main window
+                    run = False
+
 
 # Main Loop
 run = True
 while run:
-
-    window.fill(bcg)
-    word_y += speed
-    window.blit(text, (word_x,word_y))
+    window = window_creation()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:  # press any key to enter game window
+            game_window()
     if word_y > SCREEN_HEIGHT:
         text, word_x, word_y, speed = floating_words()
     pygame.display.update()
